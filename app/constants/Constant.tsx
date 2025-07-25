dcfcf7e540c7ba7f65a03736e79d5d0b3bdea716
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2025-07-25 09:06:41 +0100

    serivce done

--- app/constants/Constant.tsx
+++ app/constants/Constant.tsx
@@ -171,6 +171,73 @@ id: 6,
     body: "Through its Technical Education Unit (CICOT), Verbum Networks offers various ICT Research and Training programs. We provide hands-on training in web development, networking, cybersecurity, and more, empowering individuals with the skills needed to thrive in the digital age.",
   },
 ];
+export type Courses = {
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
+  details: string;
+};
+export const coursesCard: Courses[] = [
+  {
+    id: 1,
+    title: "Patient Management",
+    description:
+      "Easily manage patient records, history, and visits from one dashboard.",
+    icon: "/assets/photo3.jpg",
+    details: `Our Patient Management module offers a centralized platform to access, update, and organize patient information in real-time. You can securely store medical histories, allergies, diagnoses, and more in a structured and intuitive interface. It reduces manual paperwork and enhances collaboration among departments by keeping patient data synchronized and accessible across the hospital system. Staff can efficiently track patient journeys and history to ensure continuity of care. The module supports unique patient IDs, search filters, and real-time updates, ensuring that the right information is always available to authorized personnel. It is compliant with data privacy regulations to ensure maximum security.`,
+  },
+  {
+    id: 2,
+    title: "Appointments & Scheduling",
+    description:
+      "Automate doctor appointments, notifications, and calendar management.",
+    icon: "/assets/ok.jpg",
+    details: `The Appointment Scheduling feature streamlines the process of booking and managing appointments for doctors, labs, and services. Patients can easily book slots via mobile or web, while hospital staff can view and manage schedules efficiently.Automatic reminders and notifications reduce no-shows and improve patient engagement. Doctors and nurses receive real-time updates on their daily calendar, making time management more effective.The system supports walk-ins, follow-ups, cancellations, and rescheduling, making it a flexible solution for both small clinics and large hospitals. Integration with electronic health records ensures all appointments are context-aware.`,
+  },
+  {
+    id: 3,
+    title: "Medical Records",
+    description:
+      "Maintain secure and accessible digital medical records and lab reports.",
+    icon: "/assets/img4.jpg",
+    details: `Digital medical records provide a secure and paperless way to store and retrieve patient health data. This module includes prescriptions, diagnoses, lab reports, radiology images, treatment plans, and other key documents. All records are encrypted and stored in the cloud, ensuring both safety and accessibility from any authorized device. Doctors can quickly access historical data during consultations for better decision-making.Role-based access ensures that only the right personnel view or edit sensitive information. The system supports version control and audit logs to maintain data integrity and accountability.`,
+  },
+  {
+    id: 4,
+    title: "Billing & Invoicing",
+    description:
+      "Handle payments, invoices, and receipts with ease and transparency.",
+    icon: "/assets/img6.jpg",
+    details: `The Billing & Invoicing module automates the financial side of hospital management, covering services, medications, consultations, and insurance claims. It generates accurate invoices and receipts instantly.It supports multiple payment gateways, split billing, and insurance processing, allowing for a seamless financial experience for both patients and hospital staff. All transactions are recorded and auditable.
+      You can set pricing rules based on patient categories (e.g., senior citizens, insured) and generate financial reports to track revenue and outstanding balances. This helps in improving cash flow and transparency.`,
+  },
+  {
+    id: 5,
+    title: "Doctor & Staff Dashboard",
+    description:
+      "Empower staff with role-based access, duties, and reporting tools.",
+    icon: "/assets/img7.jpg",
+    details: `This dashboard gives doctors, nurses, and administrative staff a personalized view of their responsibilities. Doctors can view their schedules, appointments, patients’ records, and write prescriptions from one dashboard.
+  
+  Administrative staff can manage patient admissions, discharges, inventory, and staff shifts. It simplifies internal communication and ensures everyone has the right tools for their role.
+  
+  With role-based access control, the dashboard ensures each staff member sees only what's relevant to them. Reports and analytics offer insight into performance, enabling better planning and accountability.`,
+  },
+  {
+    id: 6,
+    title: "Hospital Analytics",
+    description:
+      "View insightful analytics and reports to improve hospital efficiency.",
+    icon: "/assets/img8.jpeg",
+    details: `The Hospital Analytics module turns raw data into actionable insights. From patient inflow trends to revenue tracking, it provides real-time dashboards to help administrators make data-driven decisions.
+  
+  You can monitor key performance indicators (KPIs) such as average wait times, patient satisfaction, and treatment success rates. The visual reports help identify bottlenecks and optimize operations.
+  
+  This module supports exporting reports to PDF/Excel and integrates with third-party BI tools. It's ideal for board meetings, audits, and long-term planning. Ultimately, it helps the hospital run smarter, not harder.`,
+  },
+];
+
 
 
 
---
+++ app/courses/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Modal from "./Modal";
+
+import { motion } from "framer-motion";
+import { coursesCard } from "../constants/Constant";
+
+const CoursesPage: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedFeature, setSelectedFeature] = useState<
+    (typeof coursesCard)[0] | null
+  >(null);
+
+  const openModal = (course: (typeof  coursesCard)[0]) => {
+    setSelectedFeature(course);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelectedFeature(null);
+  };
+
+  return (
+    <section id="features" className="py-10 bg-gray-100">
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl sm:text-3xl lg:text-4xl text-center mt-6 font-bold tracking-wide mb-8 bg">
+          Features of Vebum Health HMS
+        </h2>
+        <p className="text-color text-center mb-12 max-w-3xl mx-auto">
+          Discover the powerful features of our Hospital Management System designed to simplify administrative tasks, improve patient experience, and boost operational efficiency
+        </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          { coursesCard.map((course, index) => (
+            <motion.div
+              key={course.id}
+              onClick={() => openModal(course)}
+              className="cursor-pointer flex flex-col items-center justify-center bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition transform hover:scale-105"
+              initial={{ opacity: 0, scale: 0.9 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5, delay: index * 0.2 }}
+              viewport={{ once: true }}
+            >
+              <div className="mb-4 text-center">
+                <Image
+                  src={course.icon}
+                  alt={course.title}
+                  height={200}
+                  width={200}
+                  loading="lazy"
+                  sizes="100vw"
+                  className="w-35 h-35 rounded-full"
+                />
+              </div>
+
+              <h3 className="text-xl font-semibold text-center">
+                {course.title}
+              </h3>
+              <p className="text-color mt-2 text-center">
+                {course.description}
+              </p>
+
+              <p className="bg text-2xl font-bold text-center mt-2">
+                Click to learn more
+              </p>
+            </motion.div>
+          ))}
+        </div>
+
+        {/* Modal */}
+        {isOpen && selectedFeature && (
+          <Modal onClose={closeModal}>
+            <h2 className="text-2xl font-bold text-white">
+              {selectedFeature.title}
+            </h2>
+            <p className="mt-4 text-white">{selectedFeature.details}</p>
+          </Modal>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default CoursesPage;
